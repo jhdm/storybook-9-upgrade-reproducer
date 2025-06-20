42d53fd48cacd6d374e78fece6a523d4a536fc63
Joseph H <32443051+jhdm@users.noreply.github.com>
2025-06-20 12:19:06 -0400

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["src/**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  // { files: ["src/**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  { files: ["src/**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  globalIgnores([
+    "dist",
+  ]),
+]);
